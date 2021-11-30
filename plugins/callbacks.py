b890759a366928e144a5b3aa6489afc2560b3b7f
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-30 22:18:46 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -6,7 +6,9 @@ from pyrogram.errors import UserNotParticipant
 
 from translation import Translation
     
-    
-
-
-
+@Client.on_callback_query()
+async def button(bot, update):
+    if "|" in update.data:
+        await youtube_dl_call_back(bot, update)
+    elif "=" in update.data:
+        await ddl_call_back(bot, update)

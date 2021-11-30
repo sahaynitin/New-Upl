f57b4da09c57c488e03794443eb6597fff81a3a1
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-30 22:15:13 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -8,28 +8,5 @@ from translation import Translation
     
     
 
-@pyrogram.on_callback_query()
-async def cb_handler(bot, update):
-        
-        await update.message.delete()
 
-    elif update.data == "help":
-        await update.answer()
-        keyboard = InlineKeyboardMarkup(translation.helpbutton)
-        await update.message.edit_text(
-            text=Translation.HELP_TEXT,
-            reply_markup=keyboard,
-            disable_web_page_preview=True
-        )
 
-    elif update.data == "about":
-        await update.answer()
-        keyboard = InlineKeyboardMarkup(translation.aboutbutton)
-        await update.message.edit_text(
-            text=Translation.ABOUT_TEXT,
-            reply_markup=keyboard,
-            disable_web_page_preview=True
-        )
-
-    elif update.data == "close_data":
-        await update.message.delete()

dbd553a789185a23e9730cd2d560dae7f490e038
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-30 22:19:10 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -12,3 +12,23 @@ async def button(bot, update):
         await youtube_dl_call_back(bot, update)
     elif "=" in update.data:
         await ddl_call_back(bot, update)
+    elif update.data == "help":
+        await update.answer()
+        keyboard = InlineKeyboardMarkup(translation.helpbutton)
+        await update.message.edit_text(
+            text=Translation.HELP_TEXT,
+            reply_markup=keyboard,
+            disable_web_page_preview=True
+        )
+
+    elif update.data == "about":
+        await update.answer()
+        keyboard = InlineKeyboardMarkup(translation.aboutbutton)
+        await update.message.edit_text(
+            text=Translation.ABOUT_TEXT,
+            reply_markup=keyboard,
+            disable_web_page_preview=True
+        )
+
+    elif update.data == "close_data":
+        await update.message.delete()

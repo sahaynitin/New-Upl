869a2c9d6f1ca37f220814d30f8692c5cccf1857
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-01 07:29:44 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -1,6 +1,6 @@
 import pyrogram
 
-from pyrogram import Client as pyrogram, filters
+from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
 from pyrogram.errors import UserNotParticipant
 

6f21098e04ceb89c8c04f6f99775095c07cc1550
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-01 07:15:56 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -5,7 +5,22 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceRepl
 from pyrogram.errors import UserNotParticipant
 
 from translation import Translation
-    
+
+
+helpbutton = [[
+        InlineKeyboardButton(f'📣 Channel 📣', url="https://t.me/SDBOTs_inifinity"),
+        InlineKeyboardButton(f'🙋‍♀️ Support 🙋‍♀️', url="https://t.me/SDBOTz")
+        ],[
+        InlineKeyboardButton(f'🤖 About', callback_data="about")
+    ]]
+
+aboutbutton = [[
+        InlineKeyboardButton(f'🤔 How To Use', callback_data="help"),
+        InlineKeyboardButton(f'Close 🔐', callback_data="close_data")
+    ]]
+
+
+   
 @Client.on_callback_query()
 async def button(bot, update):
     if "|" in update.data:
@@ -14,7 +29,7 @@ async def button(bot, update):
         await ddl_call_back(bot, update)
     elif update.data == "help":
         await update.answer()
-        keyboard = InlineKeyboardMarkup(translation.helpbutton)
+        keyboard = InlineKeyboardMarkup(helpbutton)
         await update.message.edit_text(
             text=Translation.HELP_TEXT,
             reply_markup=keyboard,
@@ -23,7 +38,7 @@ async def button(bot, update):
 
     elif update.data == "about":
         await update.answer()
-        keyboard = InlineKeyboardMarkup(translation.aboutbutton)
+        keyboard = InlineKeyboardMarkup(aboutbutton)
         await update.message.edit_text(
             text=Translation.ABOUT_TEXT,
             reply_markup=keyboard,
